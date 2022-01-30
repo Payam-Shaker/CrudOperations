0614c50cc92e4d449fc5e527cc576ba88ec3f821
PayamS <72343921+Payam-Shaker@users.noreply.github.com>
2022-01-30 02:18:36 +0100

    Add project files.

---
+++ Crud.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CrudOperations
+{
+    public class Crud<T> : ICrud<T> where T : class
+    {
+        private DbContext _dbContext = null;
+        private DbSet<T> table = null;
+        public Crud(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+            table = _dbContext.Set<T>();
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return table.ToList();
+        }
+        public T GetById(object id)
+        {
+            return table.Find(id);
+        }
+        public IQueryable<T> Table { get; }
+
+        public void Delete(object id)
+        {
+            T existing = table.Find(id);
+            table.Remove(existing);
+            Save(existing);
+        }
+        public void Insert(T entity)
+        {
+            table.Add(entity);
+            Save(entity);
+        }
+        public void Update(T entity)
+        {
+            table.Attach(entity);
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            Save(entity);
+        }
+
+        public void Save(T entity)
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ ICrud.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CrudOperations
+{
+    public interface ICrud<T> where T : class
+    {
+            IEnumerable<T> GetAll();
+            T GetById(object id);
+            void Insert(T entity);
+            void Update(T entity);
+            void Delete(object id);
+            void Save(T entity);
+            IQueryable<T> Table { get; }
+    }
+}
